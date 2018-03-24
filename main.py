a9587c1f049078f9a33fa056c37e5027f607eb19
r.gow <richardstewartgow@gmail.com>
2018-03-24 13:28:31 +0000

    initial commit WIP

---
+++ main.py
@@ -0,0 +1,10 @@
+#@todo refactor into class into own file
+
+import sys
+
+class main:
+    def __init__(self):
+
+    def run(self, args):
+        arguments = sys.argv
+        processes = argumentHandler(arguments)
