4999820d9a4f3588e862f8bb46c354516f70f863
r.gow <richardstewartgow@gmail.com>
2018-03-25 17:02:33 +0100

    more work on factory

--- factories/WorkerFactory.py
+++ factories/WorkerFactory.py
@@ -3,15 +3,19 @@ class WorkerFactory:
     workerTypeMap = {}
     classMap = {'query':'QueryWorker'}
 
-    def __init__(self, commands):
-        self.commands = commands
-        self.set_up_commands()
+    def __init__(self, requests):
+        self.requests = requests
+        self.buildCounter = 0
+        self.buildLogic = {}
+        self.validate_requests()
 
-    def set_up_commands(self):
-        return True;
+    def validate_requests(self): #@todo more validation here
+        if 'buildRequests' not in self.requests and'commandRequests' not in self.requests:
+            raise Exception('Factory not given list of build and command requests')
+
+        return True
 
     def build(self):
-        for command in self.commands:
-            return True
+        self.buildCounter = self.requests['buildRequests']['workers']
 
         raise Exception('Worker Factory given no processable work')
