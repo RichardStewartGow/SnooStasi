28f1a20afdc11463e17220049ff714b89d5f4f40
r.gow <richardstewartgow@gmail.com>
2018-03-25 13:34:12 +0100

    initial work on passing arguments to the handler and added pychache to handlers modules gitignore

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 #@todo refactor into class into own file
 
-import sys, handlers.argumentHandler
+import sys
+from handlers import argumentHandler
 
 class main:
-    def __init__(self):
+    def __init__(self, arguments):
+        self.arguments = arguments
         return #@todo stub for now do more setup logic here
-    def run(self, args):
-        arguments = sys.argv
-        processes = new argumentHandler(arguments)
+    def run(self):
+        processes = argumentHandler(arguments)
+
+if __name__ == "__main__":
+    arguments = sys.argv
+    mainObject = main(arguments)
+    main.run
