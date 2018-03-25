c7ff00cc70d968f9597928b1c8c7abc2718815b4
r.gow <richardstewartgow@gmail.com>
2018-03-25 14:01:59 +0100

    started to pick out arguments and put them in a workload to process

--- handlers/argumentHandler.py
+++ handlers/argumentHandler.py
@@ -1,8 +1,20 @@
+import re
+
 
 class argumentHandler:
 
     def __init__(self, inputArguments):
         self.arguments = inputArguments
+        self.argWorkload = []
+
+    def is_argument(self, stringTarget):
+        if re.search('^--', stringTarget):
+            return True
+        return False
 
     def parse(self):
+        for argument in self.arguments:
+            if self.is_argument(argument):
+                self.argWorkload.append(argument)
+
         return False
