bd57ed638f74c5bb99daf621b520f4e7a6796fe6
r.gow <richardstewartgow@gmail.com>
2018-03-25 14:50:52 +0100

    more coding standards

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-#@todo refactor into class into own file
-
 import sys
 from handlers.ArgumentHandler import ArgumentHandler
+
 
 class Main:
     def __init__(self, args):
         self.arguments = args
+
     def run(self):
         handler = ArgumentHandler(self.arguments)
         commands = handler.parse()
