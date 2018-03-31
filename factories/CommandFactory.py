969c86432a50c62360eea616f0826bcd2aee029e
r.gow <richardstewartgow@gmail.com>
2018-03-31 12:59:36 +0100

    WIP added praw translation map

--- factories/CommandFactory.py
+++ factories/CommandFactory.py
@@ -3,6 +3,11 @@ import sys
 
 class CommandFactory:
 
+    prawTranslaslationMap = {
+        'follow-user': 'target',
+        'follow-subreddit': 'subreddit',
+        'rank-by': 'subposttype'
+    }
     classMap = {
         'follow-user': 'GetUserPostsCommand',
         'follow-subreddit': 'GetSubredditPostsCommand'
@@ -13,6 +18,7 @@ class CommandFactory:
         for command in commands:
             className = self.classMap[command]
             moduleObject = getattr(sys.modules[__name__], className)
+            ##@todo figure out why this isunt working in this context - depth?
             classObject = classObject = getattr(moduleObject, className)(
                 praw, commands
             )
